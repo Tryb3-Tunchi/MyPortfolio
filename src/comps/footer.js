3c5b2b319c9e563c5f55b384d94fd073f7262fa6
Tryb3-Tunchi <bebefinn72@gmail.com>
2024-07-08 07:51:46 -0400

    yes

---
+++ src/comps/footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="  mx-auto w-56">
+      <h2 className=" my-20 p-4"> copyright &copy; 2024</h2>
+    </div>
+  );
+};
+
+export default Footer;
